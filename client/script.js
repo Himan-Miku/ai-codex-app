1b5388b4947a028008792012a2332c5ddd0cb023
Himan-Miku <himanshu28.mj@gmail.com>
2022-12-27 23:20:28 +0530

    server deploy

--- client/script.js
+++ client/script.js
@@ -89,7 +89,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv)
 
     // Bot's Response
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://ai-codex-app.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type' : 'application/json'
